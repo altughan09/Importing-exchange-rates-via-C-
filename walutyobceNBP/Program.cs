cf6c509b060742db9001fb1a4dff8e9674be5e41
Altughan Ozengi <31768522+altugozengi@users.noreply.github.com>
2018-11-13 21:24:34 +0100

    Add files via upload

---
+++ walutyobceNBP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+namespace walutyobceNBP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load("http://www.nbp.pl/kursy/xml/a220z181113.xml");
+            XmlNodeList xnList = xml.SelectNodes("/tabela_kursow/pozycja");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Tabela kursów średnich walut obcych NBP:\n");
+            foreach (XmlNode xn in xnList)
+            {
+                string nazwaWaluty = xn["nazwa_waluty"].InnerText;
+                string przelicznik = xn["przelicznik"].InnerText;
+                string kodWaluty = xn["kod_waluty"].InnerText;
+                string kursSredni = xn["kurs_sredni"].InnerText;
+                Console.WriteLine(nazwaWaluty + " " + " " + przelicznik + " " + kodWaluty + " " + kursSredni);
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
